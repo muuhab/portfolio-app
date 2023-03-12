896c14cb179b999e2068a1e64822c7a147aa2e9c
Mohab Sherif <mohab.barca@gmail.com>
2023-03-13 01:42:31 +0200

    Handle computer model on mobile view

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,7 +4,7 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 
 import CanvasLoader from '../Loader'
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
   return (
     <mesh>
@@ -19,8 +19,8 @@ const Computers = () => {
         shadow-mapSize={1024}
       />
       <primitive object={computer.scene}
-        scale={0.75}
-        position={[0, -3.25, -1.5]}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -28,6 +28,29 @@ const Computers = () => {
 }
 
 const ComputersCanvas = () => {
+  const [isMobile, setisMobile] = useState(false)
+  useEffect(() => {
+    // Add a listner for changes to the screen size
+    const mediQuery = window.matchMedia(
+      '(max-width: 500px)'
+    )
+    // Set the initial value of isMobile 
+    setisMobile(mediQuery.matches)
+
+    // Define a callback function to handle the change to the media query
+    const handleMediaQueryChange = (e) => {
+      setisMobile(e.matches)
+    }
+
+    // Add the callback function to the listner for changes to the media query
+    mediQuery.addEventListener('change', handleMediaQueryChange)
+
+    // Remove the event listener when the component unmounts
+    return () => {
+      mediQuery.removeEventListener('change', handleMediaQueryChange)
+    }
+  }, [])
+
   return (
     <Canvas frameloop='demand' shadows camera={{ position: [20, 3, 5], fov: 25 }} gl={{ preserveDrawingBuffer: true }}>
       <Suspense fallback={<CanvasLoader />}>
@@ -36,7 +59,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
       <Preload all />
     </Canvas>
